13a02de972f628d181c381191a172b3eab039224
in_drops <indropschannel@gmail.com>
2025-03-12 08:23:37 +0300

    commits 12.03.25

--- OGLabsSwapsMain.py
+++ OGLabsSwapsMain.py
@@ -72,8 +72,9 @@ def activity(bot: Bot):
 
     while swaps < random_count:
         for token_out in tokens_out:
-            if no_balance_tokens == len(tokens_out):
+            if no_balance_tokens == 9:
                 logger.error("Нет токенов с балансом для свапов!")
+                logger.success(f'Выполнено свапов: {random_count}! Данные записаны в таблицу OGLabsActivity.xlsx')
                 return
             random_sleep(5, 10)
             bot.ads.page.locator("span.material-icons-round", has_text="expand_more").nth(0).click()
@@ -121,7 +122,7 @@ def activity(bot: Bot):
                 swaps += 1
 
             if swaps >= random_count:
-                logger.success(f'Выполнено {random_count} свапов! Данные записаны в таблицу OGLabsActivity.xlsx')
+                logger.success(f'Выполнено свапов: {random_count}! Данные записаны в таблицу OGLabsActivity.xlsx')
                 break
 
 
