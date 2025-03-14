fd9884e56770378fa7d666f9dba7b42cda02e1b0
in_drops <indropschannel@gmail.com>
2025-03-14 21:03:00 +0300

    commits 14.03.25

---
+++ MintAuraNFT_1.py
@@ -0,0 +1,60 @@
+import random
+from loguru import logger
+from config import config, Chains
+from core.bot import Bot
+from core.excel import Excel
+from core.onchain import Onchain
+from models.account import Account
+from models.amount import Amount
+from utils.inputs import input_pause
+from utils.logging import init_logger
+from utils.utils import (random_sleep, get_accounts, select_profiles, get_user_agent)
+
+
+def main():
+
+    init_logger()
+    accounts = get_accounts()
+    accounts_for_work = select_profiles(accounts)
+    pause = input_pause()
+
+    for i in range(config.cycle):
+        random.shuffle(accounts_for_work)
+        for account in accounts_for_work:
+            worker(account)
+            random_sleep(pause)
+        logger.success(f'Цикл {i + 1} завершен, обработано {len(accounts_for_work)} аккаунтов!')
+        logger.info(f'Ожидание перед следующим циклом ~{config.pause_between_cycle[1]} секунд!')
+        random_sleep(*config.pause_between_cycle)
+
+def worker(account: Account) -> None:
+
+    try:
+        with Bot(account) as bot:
+            activity(bot)
+    except Exception as e:
+        logger.critical(f"{account.profile_number} Ошибка при инициализации Bot: {e}")
+
+def activity(bot: Bot):
+
+    get_user_agent()
+    excel_report = Excel(bot.account, file='OGLabsActivity.xlsx')
+    excel_report.set_cell('Address', f'{bot.account.address}')
+    excel_report.set_date('Date')
+    bot.onchain.change_chain(Chains.OGLABS_TESTNET)
+    mintaura_address = '0x8260aBAd9079FE6B50fD9248D5996f810Fe01ceF'
+    tx = bot.onchain._prepare_tx(to_address=mintaura_address)
+    tx['data'] = f'0x84bb1e42000000000000000000000000{bot.account.address[2:]}0000000000000000000000000000000000000000000000000000000000000001000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c000000000000000000000000000000000000000000000000000000000000001800000000000000000000000000000000000000000000000000000000000000080ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+    bot.onchain._estimate_gas(tx)
+    tx_hash = bot.onchain._sign_and_send(tx)
+    logger.info(f'Транзакция отправлена! Данные занесены в таблицу OGLabsActivity.xls! Hash: {tx_hash}')
+    excel_report.increase_counter(f'MintAura NFT #1')
+
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        logger.warning('Программа завершена вручную!')
